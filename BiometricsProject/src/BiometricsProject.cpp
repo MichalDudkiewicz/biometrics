6ac88c226d5e2519b5a12857068c9db8aad18bfe
MichalDudkiewicz <michal.dudkiewicz96@gmail.com>
2022-03-23 19:23:29 +0100

    cleanup

---
+++ BiometricsProject/src/BiometricsProject.cpp
@@ -0,0 +1,11 @@
+﻿#include "BiometricsProject.h"
+#include "EasyBMP/EasyBMP.h"
+
+int main()
+{
+	std::cout << "Hello biometrics" << std::endl;
+	BMP Image;
+	Image.SetSize(400, 400);
+	Image.WriteToFile("test.bmp");
+	return 0;
+}
