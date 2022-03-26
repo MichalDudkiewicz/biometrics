3b6579136644e0958f41517be3a6e158a1ae072b
MichalDudkiewicz <michal.dudkiewicz96@gmail.com>
2022-03-26 23:45:26 +0100

    linking opencv

--- BiometricsProject/src/BiometricsProject.cpp
+++ BiometricsProject/src/BiometricsProject.cpp
@@ -1,8 +1,13 @@
 ﻿#include "BiometricsProject.h"
 #include "EasyBMP/EasyBMP.h"
+#include <opencv2/opencv.hpp>
 
 int main()
 {
+    cv::Mat image;
+    namedWindow("Display Image", cv::WINDOW_AUTOSIZE );
+    imshow("Display Image", image);
+    cv::waitKey(0);
 	std::cout << "Hello biometrics" << std::endl;
 	BMP Image;
 	Image.SetSize(400, 400);
