fba8415f77e8c20ce620a3d5940c8878faf29e1b
MichalDudkiewicz <michal.dudkiewicz96@gmail.com>
2022-03-30 19:37:48 +0200

    cleanup

--- BiometricsProject/src/BiometricsProject.cpp
+++ BiometricsProject/src/BiometricsProject.cpp
@@ -15,15 +15,7 @@ int main()
 	cv::imshow("Display Image2", img);
     cv::waitKey(0);
 
-//	cv::cvtColor(img, out);
-
-	//tried cropping
-	//cv::Rect crop = cv::boundingRect(out);
-	//cv::Mat cropped_img = out(cv::Range(crop.y, crop.y + crop.height), cv::Range(crop.x, crop.x + crop.width));
-	//cv::imshow("Display Image", cropped_img);
-	//cv::waitKey(0);
-
-    cv::medianBlur(img, out, 3);
+    cv::medianBlur(img, out, 5);
     cv::imshow("Display Image3", out);
     cv::waitKey(0);
 
@@ -46,23 +38,6 @@ int main()
     cv::bitwise_not(out, img);
     cv::imshow("Display Image8", img);
     cv::waitKey(0);
-
-//	cv::morphologyEx(img, out, cv::MORPH_OPEN, cv::getStructuringElement(1 ,cv::Size(3,3), cv::Point(1,1)));
-//	cv::imshow("Display Image5", out);
-//	cv::waitKey(0);
-//
-//	cv::dilate(out, img, cv::getStructuringElement(cv::MORPH_ELLIPSE, cv::Size(3, 3), cv::Point(2, 2)));
-//	cv::imshow("Display Image6", img);
-//	cv::waitKey(0);
-//
-//	cv::dilate(img, out, cv::getStructuringElement(cv::MORPH_ELLIPSE, cv::Size(2, 2), cv::Point(1, 1)));
-//	cv::imshow("Display Image7", out);
-//	cv::waitKey(0);
-
-	//cv::Laplacian(out, img, CV_16S, 3, 1, 0, cv::BORDER_DEFAULT);
-	//cv::convertScaleAbs(img, out);
-	//cv::imshow("Display Image", out);
-	//cv::waitKey(0);
 
 	return 0;
 }
