b7f15a553a826d50d7094ac8731046c4dc42109e
MichalDudkiewicz <michal.dudkiewicz96@gmail.com>
2022-05-05 18:28:33 +0200

    gabor

--- BiometricsProject/src/BiometricsProject.cpp
+++ BiometricsProject/src/BiometricsProject.cpp
@@ -4,6 +4,12 @@
 #include <opencv2/opencv.hpp>
 #include <opencv2/ximgproc.hpp>
 #include <tuple>
+#include <numeric>
+
+namespace
+{
+    constexpr int kSobelSize = 3;
+}
 
 enum MinutiaeType
 {
@@ -177,57 +183,131 @@ std::vector<std::tuple<Pixel, MinutiaeType, MinutiaeDirection>> retrieveMinutiae
     return minutiaes;
 }
 
-double getOrientation(const std::vector<cv::Point>& pts)
+double Variance(std::vector<double> samples)
 {
-    //Construct a buffer used by the pca analysis
-    int sz = static_cast<int>(pts.size());
-    cv::Mat data_pts = cv::Mat(sz, 2, CV_64F);
-    for (int i = 0; i < data_pts.rows; i++)
-    {
-        data_pts.at<double>(i, 0) = pts[i].x;
-        data_pts.at<double>(i, 1) = pts[i].y;
+    const size_t sz = samples.size();
+    if (sz == 1) {
+        return 0.0;
     }
-    //Perform PCA analysis
-    cv::PCA pca_analysis(data_pts, cv::Mat(), cv::PCA::DATA_AS_ROW);
-    //Store the center of the object
-    cv::Point cntr = cv::Point(static_cast<int>(pca_analysis.mean.at<double>(0, 0)),
-        static_cast<int>(pca_analysis.mean.at<double>(0, 1)));
-    //Store the eigenvalues and eigenvectors
-    std::vector<cv::Point2d> eigen_vecs(2);
-    std::vector<double> eigen_val(2);
-    for (int i = 0; i < 2; i++)
-    {
-        eigen_vecs[i] = cv::Point2d(pca_analysis.eigenvectors.at<double>(i, 0),
-            pca_analysis.eigenvectors.at<double>(i, 1));
-        eigen_val[i] = pca_analysis.eigenvalues.at<double>(i);
-    }
-    double angle = atan2(eigen_vecs[0].y, eigen_vecs[0].x); // orientation in radians
-    angle = (angle * 180.f) / CV_PI;
-    std::cout << angle << std::endl;
-    return angle;
+
+    // Calculate the mean
+    const double mean = std::accumulate(samples.begin(), samples.end(), 0.0) / sz;
+
+    // Now calculate the variance
+    auto variance_func = [&mean, &sz](double accumulator, const double& val) {
+        return accumulator + ((val - mean)*(val - mean) / (sz - 1));
+    };
+
+    return std::accumulate(samples.begin(), samples.end(), 0.0, variance_func);
 }
 
-cv::Mat gabor(cv::Mat& myImg){
+double StandardDeviation(std::vector<double> samples)
+{
+    return sqrt(Variance(samples));
+}
+
+cv::Mat gabor(cv::Mat& myImg, const std::vector<std::vector<std::optional<double>>>& orientationMatrix){
     // prepare the output matrix for filters
-    cv::Mat enhanced(myImg.rows, myImg.cols, CV_32F);
+    cv::Mat img(myImg.rows, myImg.cols, CV_32F);
+    img = cv::Scalar(0);
 
     //predefine parameters for Gabor kernel
-    cv::Size kSize(31, 31);
+    int k = 30;
+    cv::Size kSize(k, k);
+    const int sobelInK = k / kSobelSize;
 
-    double lambda = 8;
+    double lambda = 10;
     double sigma = 4;
-    double gamma = 0.6;
-    double psi =  0.1;
+    double gamma = 1;
 
-    for (double theta = 0.0; theta <= 180.0; theta += 1.0)
+    std::optional<double> previousTheta;
+
+    for (int r = 0; r < orientationMatrix.size() - sobelInK; r+=sobelInK)
     {
-        cv::Mat kernel = cv::getGaborKernel(kSize, sigma, theta, lambda, gamma, psi);
-        cv::Mat gabor(myImg.rows, myImg.cols, CV_32F);
-        cv::filter2D(myImg, gabor, CV_32F, kernel);
-        cv::add(enhanced, gabor, enhanced);
+        for (int c = 0; c < orientationMatrix[r].size() - sobelInK; c+=sobelInK)
+        {
+            double sum = 0.0;
+            std::vector<double> dirs;
+            for (int i = r; i < orientationMatrix.size() && i < r + sobelInK; i++)
+            {
+                for (int j = c; j < orientationMatrix[r].size() && j < c + sobelInK; j++)
+                {
+                    const auto dir = orientationMatrix[i][j];
+                    if (dir.has_value())
+                    {
+                        sum += dir.value();
+                        dirs.push_back(dir.value());
+                    }
+                }
+            }
+
+            if (dirs.size() > 0)
+            {
+                const auto stdDev = StandardDeviation(dirs);
+                if (stdDev >1.5)
+                {
+                    const int s = 3;
+                    for (int i = r; i < orientationMatrix.size() && i < r + sobelInK; i++)
+                    {
+                        for (int j = c; j < orientationMatrix[r].size() && j < c + sobelInK; j++)
+                        {
+                            const auto dir = orientationMatrix[i][j];
+                            if (dir.has_value())
+                            {
+                                double theta = dir.value();
+                                cv::Mat kernel = cv::getGaborKernel(cv::Size(s, s), sigma, theta, lambda, gamma);
+                                cv::Mat gabor(s, s, CV_32F);
+                                cv::filter2D(myImg(cv::Range(i * kSobelSize,i * kSobelSize + s), cv::Range(j * kSobelSize, j * kSobelSize + s)), gabor, CV_32F, kernel);
+                                cv::Mat aux = img.rowRange(i * kSobelSize,i * kSobelSize + s).colRange(j * kSobelSize, j * kSobelSize + s);
+                                gabor.copyTo(aux);
+                            }
+                        }
+                    }
+                }
+                else
+                {
+                    const double theta = sum / dirs.size();
+                    cv::Mat kernel = cv::getGaborKernel(kSize, sigma, theta, lambda, gamma);
+                    cv::Mat gabor(k, k, CV_32F);
+                    cv::filter2D(myImg(cv::Range(r * kSobelSize,r * kSobelSize + k), cv::Range(c * kSobelSize, c * kSobelSize + k)), gabor, CV_32F, kernel);
+                    cv::Mat aux = img.rowRange(r * kSobelSize,r * kSobelSize + k).colRange(c * kSobelSize, c * kSobelSize + k);
+                    gabor.copyTo(aux);
+
+                    if (previousTheta.has_value() && abs(previousTheta.value() - theta) > 0.5)
+                    {
+
+                        double sum2 = 0.0;
+                        std::vector<double> dirs2;
+                        for (int i = r - sobelInK/2; i < orientationMatrix.size() && i < r + sobelInK/2; i++)
+                        {
+                            for (int j = c - sobelInK/2; j < orientationMatrix[r].size() && j < c + sobelInK/2; j++)
+                            {
+                                const auto dir = orientationMatrix[i][j];
+                                if (dir.has_value())
+                                {
+                                    sum2 += dir.value();
+                                    dirs2.push_back(dir.value());
+                                }
+                            }
+                        }
+                        if (!dirs2.empty())
+                        {
+                            const double theta2 = sum2 / dirs2.size();
+                            cv::Mat kernel2 = cv::getGaborKernel(kSize, sigma, theta2, lambda, gamma);
+                            cv::Mat gabor2(k, k, CV_32F);
+                            cv::filter2D(myImg(cv::Range(r * kSobelSize - k/2,r * kSobelSize + k/2), cv::Range(c * kSobelSize - k/2, c * kSobelSize + k/2)), gabor2, CV_32F, kernel2);
+                            cv::Mat aux2 = img.rowRange(r * kSobelSize - k/2,r * kSobelSize + k/2).colRange(c * kSobelSize - k/2, c * kSobelSize + k/2);
+                            gabor2.copyTo(aux2);
+                        }
+
+                    }
+                    previousTheta = theta;
+                }
+            }
+        }
     }
 
-    return enhanced;
+    return img;
 }
 
 
@@ -244,15 +324,84 @@ int main()
 
     buffer = img;
 
-    out = gabor(buffer);
+    const auto clahe = cv::createCLAHE();
+    clahe->apply(buffer, out);
+    cv::imshow("Contrast stretching", out);
+    cv::waitKey(0);
+    buffer = out;
+
+    cv::GaussianBlur(buffer, out, cv::Size(3,3), 0);
+//    cv::medianBlur(buffer, out, 5);
+    cv::imshow("Blur", out);
+    cv::waitKey(0);
+    buffer = out;
+
+    // orientation image
+    const int k = kSobelSize;
+    std::vector<std::vector<std::optional<double>>> orientationMatrix;
+    for (int i = 0; i < buffer.rows / k - 1; i++)
+    {
+        int y = i * k + k/2;
+        orientationMatrix.emplace_back();
+        for (int j = 0; j < buffer.cols / k - 1; j++)
+        {
+            int x = j * k + k/2;
+            cv::Mat cropped = buffer(cv::Range(y - k/2,y + k/2 + 1), cv::Range(x - k/2,x + + k/2 + 1));
+            cv::Mat grad_x;
+            cv::Scharr(cropped, grad_x, CV_32F, 1, 0, k);
+            cv::Mat grad_y;
+            cv::Scharr(cropped, grad_y, CV_32F, 0, 1, k);
+
+            cv::Scalar gx = cv::sum(grad_x);
+            cv::Scalar gy = cv::sum(grad_y);
+
+            const double Gx = gx.val[0];
+            const double Gy = gy.val[0];
+            std::optional<double> theta;
+            if (Gx != 0 || Gy != 0)
+            {
+                theta = atan2(Gy, Gx);
+                if (theta.value() < 0)
+                {
+                    theta.value() += M_PI;
+                }
+            }
+
+            orientationMatrix[i].push_back(theta);
+        }
+    }
+
+
+    out = gabor(buffer, orientationMatrix);
     cv::imshow("Gabor", out);
     cv::waitKey(0);
     out.convertTo(buffer, CV_8UC1);
+    cv::imshow("Conv", buffer);
+    cv::waitKey(0);
 
-    cv::bitwise_not(buffer, buffer);
+//    cv::GaussianBlur(buffer, out, cv::Size(3,3), 0);
+//    cv::imshow("Blur", out);
+//    cv::waitKey(0);
+//    buffer = out;
+
+//    const auto clahe = cv::createCLAHE(40.0, cv::Size(90, 90));
+//    clahe->apply(buffer, out);
+//    cv::imshow("Contrast stretching", out);
+//    cv::waitKey(0);
+//    buffer = out;
+
+//    const auto ke = cv::getStructuringElement(cv::MORPH_ELLIPSE, cv::Size(3, 3));
+//    cv::morphologyEx(buffer, out, cv::MORPH_OPEN, ke);
+//    cv::imshow("Blur", out);
+//    cv::waitKey(0);
+//    buffer = out;
+
+//    cv::bitwise_not(buffer, buffer);
+//    cv::imshow("Thinned1", buffer);
+    cv::waitKey(0);
     cv::ximgproc::thinning(buffer, out, cv::ximgproc::ThinningTypes::THINNING_ZHANGSUEN);
     cv::bitwise_not(out, out);
-    cv::imshow("Thinned", out);
+    cv::imshow("Thinned2", out);
     cv::waitKey(0);
 
     auto minutiaes = retrieveMinutiaes(out);
