59ebdefc94a792ee725b070d3902538a639ddc7e
VaRoXXas <kmichniewski@gmail.com>
2022-05-18 01:43:54 +0200

    reading wav file

--- BiometricsProject_VoiceRecognition/src/VoiceRecognition.cpp
+++ BiometricsProject_VoiceRecognition/src/VoiceRecognition.cpp
@@ -1,8 +1,4 @@
-#include <aquila/global.h>
-#include <aquila/source/generator/SineGenerator.h>
-#include <aquila/transform/FftFactory.h>
-#include <aquila/tools/TextPlot.h>
-#include <aquila/functions.h>
+#include <aquila/aquila.h>
 
 int main()
 {
@@ -11,18 +7,25 @@ int main()
     const Aquila::FrequencyType sampleFreq = 2000;
     const Aquila::FrequencyType f1 = 125, f2 = 700;
 
+    Aquila::WaveFile sound("E.wav");
+    
+    //Aquila::WaveFileHandler reader("gen.wav");
+    //reader.save(sound);
+
     Aquila::SineGenerator sineGenerator1 = Aquila::SineGenerator(sampleFreq);
     sineGenerator1.setAmplitude(32).setFrequency(f1).generate(SIZE);
     Aquila::SineGenerator sineGenerator2 = Aquila::SineGenerator(sampleFreq);
     sineGenerator2.setAmplitude(8).setFrequency(f2).setPhase(0.75).generate(SIZE);
     auto sum = sineGenerator1 + sineGenerator2;
-
     Aquila::TextPlot plt("Input signal");
     plt.plot(sum);
+    
+    //reader.save(sound);
+    Aquila::WaveFile::save(sound, "test.wav");
 
     // calculate the FFT
     auto fft = Aquila::FftFactory::getFft(SIZE);
-    Aquila::SpectrumType spectrum = fft->fft(sum.toArray());
+    Aquila::SpectrumType spectrum = fft->fft(sound.toArray());
 
     plt.setTitle("Spectrum");
     plt.plotSpectrum(spectrum);
