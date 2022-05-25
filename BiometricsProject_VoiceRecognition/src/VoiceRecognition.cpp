6837f9f74d88036cf143a278293e98a4f053e54c
VaRoXXas <kmichniewski@gmail.com>
2022-05-25 03:46:44 +0200

    sound processing
    - Hamming window
    - spliting into smaller fragments
    - emphasis
    - MFCC
    - comparison of two sounds

--- BiometricsProject_VoiceRecognition/src/VoiceRecognition.cpp
+++ BiometricsProject_VoiceRecognition/src/VoiceRecognition.cpp
@@ -1,34 +1,56 @@
 #include <aquila/aquila.h>
+#include <aquila/tools/TextPlot.h>
+
+Aquila::DtwDataType ProcessSound(Aquila::SignalSource soundToProcess, size_t sampleSize)
+{
+    //okienkowanie
+    Aquila::HammingWindow hamming(soundToProcess.getSamplesCount());
+    soundToProcess *= hamming;
+
+    //podzia³ na fragmenty
+    Aquila::FramesCollection frameCollection(soundToProcess, sampleSize);
+
+    //preemfaza ?
+    float alpha = 0.95;
+    for (int i = 0; i < frameCollection.count()-1; i++)
+    {
+        frameCollection.frame(i + 1) += (-alpha * frameCollection.frame(i));
+    }
+
+    //wykonanie MFCC na ka¿dym fragmencie
+    Aquila::DtwDataType dtwdt;
+    Aquila::Mfcc mfcc0(sampleSize);
+    for (int i = 0; i < frameCollection.count(); i++)
+    {
+        Aquila::Frame frame = frameCollection.frame(i);
+        std::vector<double> mfccValues = mfcc0.calculate(frame);
+        dtwdt.push_back(mfccValues);
+    }
+
+    //obliczone wspó³czynniki MFCC zwracamy w formie wektora
+    return dtwdt;
+}
 
 int main()
 {
     // input signal parameters
     const std::size_t SIZE = 64;
-    const Aquila::FrequencyType sampleFreq = 2000;
-    const Aquila::FrequencyType f1 = 125, f2 = 700;
-
-    Aquila::WaveFile sound("E.wav");
     
-    //Aquila::WaveFileHandler reader("gen.wav");
-    //reader.save(sound);
-
-    Aquila::SineGenerator sineGenerator1 = Aquila::SineGenerator(sampleFreq);
-    sineGenerator1.setAmplitude(32).setFrequency(f1).generate(SIZE);
-    Aquila::SineGenerator sineGenerator2 = Aquila::SineGenerator(sampleFreq);
-    sineGenerator2.setAmplitude(8).setFrequency(f2).setPhase(0.75).generate(SIZE);
-    auto sum = sineGenerator1 + sineGenerator2;
-    Aquila::TextPlot plt("Input signal");
-    plt.plot(sum);
+    Aquila::WaveFile sound_1("FAML_Sa.wav");
     
-    //reader.save(sound);
-    Aquila::WaveFile::save(sound, "test.wav");
+    Aquila::DtwDataType data_1 = ProcessSound(sound_1, SIZE);
 
-    // calculate the FFT
-    auto fft = Aquila::FftFactory::getFft(SIZE);
-    Aquila::SpectrumType spectrum = fft->fft(sound.toArray());
 
-    plt.setTitle("Spectrum");
-    plt.plotSpectrum(spectrum);
+    Aquila::WaveFile sound_2("MCBR_Sa.wav");
+
+    Aquila::DtwDataType data_2 = ProcessSound(sound_2, SIZE);
+
+    //porównanie dŸwiêków
+    Aquila::Dtw dtw(Aquila::euclideanDistance, Aquila::Dtw::PassType::Diagonals);
+    
+    double distance_1 = dtw.getDistance(data_1, data_2);
+    std::cout << "Distance : " << distance_1 << std::endl;
+
 
     return 0;
 }
