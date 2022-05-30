3e12bab6264d46480c8a6b0fcd3e750b5676777e
VaRoXXas <kmichniewski@gmail.com>
2022-05-30 21:45:36 +0200

    Low-pass filter

--- BiometricsProject_VoiceRecognition/src/VoiceRecognition.cpp
+++ BiometricsProject_VoiceRecognition/src/VoiceRecognition.cpp
@@ -34,22 +34,69 @@ Aquila::DtwDataType ProcessSound(Aquila::SignalSource soundToProcess, size_t sam
 int main()
 {
     // input signal parameters
-    const std::size_t SIZE = 64;
-    
+    //const std::size_t SIZE = 64;
+    const Aquila::FrequencyType f_lp = 500;
+
     Aquila::WaveFile sound_1("FAML_Sa.wav");
+
+    const std::size_t SIZE = 65536;
+    const Aquila::FrequencyType sampleFreq = sound_1.getSampleFrequency();
+
+    auto fft = Aquila::FftFactory::getFft(SIZE);
+    Aquila::SpectrumType spectrum = fft->fft(sound_1.toArray());
+
+    // generowanie spektrum filtru
+    Aquila::SpectrumType filterSpectrum(SIZE);
+    for (std::size_t i = 0; i < SIZE; ++i)
+    {
+        if (i < (SIZE * f_lp / sampleFreq))
+        {
+            // passband
+            filterSpectrum[i] = 1.0;
+        }
+        else
+        {
+            // stopband
+            filterSpectrum[i] = 0.0;
+        }
+    }
     
-    Aquila::DtwDataType data_1 = ProcessSound(sound_1, SIZE);
+    std::transform(
+        std::begin(spectrum),
+        std::end(spectrum),
+        std::begin(filterSpectrum),
+        std::begin(spectrum),
+        [](Aquila::ComplexType x, Aquila::ComplexType y) { return x * y; }
+    );
 
-
-    Aquila::WaveFile sound_2("MCBR_Sa.wav");
-
-    Aquila::DtwDataType data_2 = ProcessSound(sound_2, SIZE);
-
-    //porównanie dŸwiêków
-    Aquila::Dtw dtw(Aquila::euclideanDistance, Aquila::Dtw::PassType::Diagonals);
+    //std::vector<Aquila::SampleType> x1;
+    double x1[SIZE];
+    fft->ifft(spectrum, x1);
     
-    double distance_1 = dtw.getDistance(data_1, data_2);
-    std::cout << "Distance : " << distance_1 << std::endl;
+    Aquila::SignalSource lowPassSound(x1,SIZE,sampleFreq);
+    
+    Aquila::WaveFile::save(lowPassSound,"newSound.wav");
+
+    //podzia³ na fragmenty
+    Aquila::FramesCollection frameCollection(sound_1, SIZE);
+
+    for (int i = 0; i < frameCollection.count(); i++)
+    {
+        std::cout << sound_1.getAudioLength() << std::endl;
+    }
+
+    //Aquila::DtwDataType data_1 = ProcessSound(sound_1, SIZE);
+
+
+    //Aquila::WaveFile sound_2("MCBR_Sa.wav");
+
+    //Aquila::DtwDataType data_2 = ProcessSound(sound_2, SIZE);
+
+    ////porównanie dŸwiêków
+    //Aquila::Dtw dtw(Aquila::euclideanDistance, Aquila::Dtw::PassType::Diagonals);
+    //
+    //double distance_1 = dtw.getDistance(data_1, data_2);
+    //std::cout << "Distance : " << distance_1 << std::endl;
 
 
     return 0;
