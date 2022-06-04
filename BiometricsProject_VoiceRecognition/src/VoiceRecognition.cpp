179a5be14fb1bb00cb42822cfd84a10a38a38ce6
VaRoXXas <kmichniewski@gmail.com>
2022-06-05 01:18:46 +0200

    Tweaked recognition algorithm + read files from directory

--- BiometricsProject_VoiceRecognition/src/VoiceRecognition.cpp
+++ BiometricsProject_VoiceRecognition/src/VoiceRecognition.cpp
@@ -1,55 +1,61 @@
+#include <filesystem>
 #include <aquila/aquila.h>
 #include <aquila/tools/TextPlot.h>
 
-Aquila::DtwDataType ProcessSound(Aquila::SignalSource soundToProcess, size_t sampleSize)
+constexpr std::size_t FrameSize = 1024;
+
+int ZCR(Aquila::Frame signal)
 {
-    //okienkowanie
-    Aquila::HammingWindow hamming(soundToProcess.getSamplesCount());
-    soundToProcess *= hamming;
-
-    //podzia³ na fragmenty
-    Aquila::FramesCollection frameCollection(soundToProcess, sampleSize);
-
-    //preemfaza ?
-    float alpha = 0.95;
-    for (int i = 0; i < frameCollection.count()-1; i++)
+    int crossingPoints = 0;
+    for (int i = 0; i < signal.getSamplesCount() - 1; i++)
     {
-        frameCollection.frame(i + 1) += (-alpha * frameCollection.frame(i));
+        if (!signbit(signal.toArray()[i + 1]) != !signbit(signal.toArray()[i]))
+        crossingPoints++;
     }
-
-    //wykonanie MFCC na ka¿dym fragmencie
-    Aquila::DtwDataType dtwdt;
-    Aquila::Mfcc mfcc0(sampleSize);
-    for (int i = 0; i < frameCollection.count(); i++)
-    {
-        Aquila::Frame frame = frameCollection.frame(i);
-        std::vector<double> mfccValues = mfcc0.calculate(frame);
-        dtwdt.push_back(mfccValues);
-    }
-
-    //obliczone wspó³czynniki MFCC zwracamy w formie wektora
-    return dtwdt;
+    return crossingPoints;
 }
 
-int main()
+float CalculateFrequencyFromCrossingPoints(int crossingPoints, std::size_t samplesCount, Aquila::FrequencyType sampleFrequency)
 {
-    // input signal parameters
-    //const std::size_t SIZE = 64;
-    const Aquila::FrequencyType f_lp = 500;
+    float signalLength = samplesCount / sampleFrequency;
 
-    Aquila::WaveFile sound_1("100hz.wav");
+    int oscillations = crossingPoints / 2;
 
-    const std::size_t SIZE = 65536;
-    const Aquila::FrequencyType sampleFreq = sound_1.getSampleFrequency();
+    return oscillations / signalLength;
+}
 
-    auto fft = Aquila::FftFactory::getFft(SIZE);
-    Aquila::SpectrumType spectrum = fft->fft(sound_1.toArray());
+float Median(std::vector<float> values)
+{
+    size_t size = values.size();
+
+    if (size == 0)
+    {
+        return 0;  // Undefined
+    }
+    else
+    {
+        sort(values.begin(), values.end());
+        if (size % 2 == 0)
+        {
+            return (values[size / 2 - 1] + values[size / 2]) / 2;
+        }
+        else
+        {
+            return values[size / 2];
+        }
+    }
+}
+
+Aquila::Frame LowPassFilter(Aquila::Frame signal, Aquila::FrequencyType filter, Aquila::FrequencyType sampleFrequency, Aquila::SignalSource* CopySignal)
+{
+    auto fft = Aquila::FftFactory::getFft(signal.getSamplesCount());
+    Aquila::SpectrumType spectrum = fft->fft(signal.toArray());
 
     // generowanie spektrum filtru
-    Aquila::SpectrumType filterSpectrum(SIZE);
-    for (std::size_t i = 0; i < SIZE; ++i)
+    Aquila::SpectrumType filterSpectrum(signal.getSamplesCount());
+    for (std::size_t i = 0; i < signal.getSamplesCount(); ++i)
     {
-        if (i < (SIZE * f_lp / sampleFreq))
+        if (i < (signal.getSamplesCount() * filter / signal.getSampleFrequency()))
         {
             // passband
             filterSpectrum[i] = 1.0;
@@ -60,7 +66,7 @@ int main()
             filterSpectrum[i] = 0.0;
         }
     }
-    
+
     std::transform(
         std::begin(spectrum),
         std::end(spectrum),
@@ -69,38 +75,56 @@ int main()
         [](Aquila::ComplexType x, Aquila::ComplexType y) { return x * y; }
     );
 
-    double x1[SIZE];
+    double x1[FrameSize];
     fft->ifft(spectrum, x1);
-    
-    Aquila::SignalSource lowPassSound(x1,SIZE,sampleFreq);
-    
-    Aquila::WaveFile::save(lowPassSound,"newSound.wav");
 
-    //podzia³ na fragmenty
-    //Aquila::FramesCollection frameCollection(sound_1, SIZE);
-    int crossingPoints = 0;
+    CopySignal = new Aquila::SignalSource(x1, signal.getSamplesCount(), signal.getSampleFrequency());
+    Aquila::Frame f(*CopySignal, CopySignal->begin().getPosition(), CopySignal->end().getPosition());
+    return f;
+}
 
-    for (int i = 0; i < SIZE-1; i++)
+int main()
+{
+    // input signal parameters
+    //const std::size_t SIZE = 64;
+    const Aquila::FrequencyType f_lp = 400;
+
+    std::vector<Aquila::WaveFile> databaseFiles;
+
+    std::string path = "./database";
+
+    for (const auto& file : std::filesystem::directory_iterator::directory_iterator(path))
     {
-        if (!signbit(x1[i + 1]) != !signbit(x1[i])) 
-            crossingPoints++;
+        std::cout << file.path() << std::endl;
+        databaseFiles.push_back(Aquila::WaveFile(file.path().string()));
+    }
+
+    for (Aquila::WaveFile file : databaseFiles)
+    {
+        //podzia³ na fragmenty - po 64 milisekundy
+        Aquila::FramesCollection frameCollection(file, FrameSize);
+        std::vector<float> frequencies;
+        for (int i = 0; i < frameCollection.count(); i++)
+        {
+            Aquila::SignalSource* signal = new Aquila::SignalSource(file.getSampleFrequency());
+            Aquila::Frame frame = LowPassFilter(frameCollection.frame(i), f_lp, file.getSampleFrequency(), signal);
+            int crossingPoints = ZCR(frame);
+            float calculatedFrequency = CalculateFrequencyFromCrossingPoints(crossingPoints, frame.getSamplesCount(), frame.getSampleFrequency());
+            frequencies.push_back(calculatedFrequency);
+        }
+
+
+        std::cout << Median(frequencies) << std::endl;
     }
 
 
-    float signalLength = lowPassSound.getSamplesCount() / lowPassSound.getSampleFrequency();
 
-    int oscillations = crossingPoints / 2;
-
-    int calculatedFrequency = oscillations / signalLength;
-
-    //std::cout << oscillations << std::endl;
-
-    if(calculatedFrequency > 60 && calculatedFrequency < 180)
-        std::cout << "Speaker gender is Male and frequency is: " << calculatedFrequency << std::endl;
-    else if(calculatedFrequency > 180 && calculatedFrequency < 300)
-        std::cout << "Speaker gender is Female and frequency is: " << calculatedFrequency << std::endl;
-    else
-    std::cout << "Frequency is: " << calculatedFrequency << std::endl;
+    //if(calculatedFrequency > 60 && calculatedFrequency < 180)
+    //    std::cout << "Speaker gender is Male and frequency is: " << calculatedFrequency << std::endl;
+    //else if(calculatedFrequency > 180 && calculatedFrequency < 300)
+    //    std::cout << "Speaker gender is Female and frequency is: " << calculatedFrequency << std::endl;
+    //else
+    //std::cout << "Frequency is: " << calculatedFrequency << std::endl;
 
     return 0;
 }
